2571fc4b8a06039e9d7ad18dfcb80786fd2b632c
Mahbubur Rahman <mail@mahbub.me>
2019-01-04 16:58:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var instrumentsPlayByMusicians = [];
+  
+  for(var i = 0; i < musicians.length; i++) {
+    instrumentsPlayByMusicians.push(`${musicians[i]} plays ${instruments[i]}`);     
+  }
+  return instrumentsPlayByMusicians;
+}
+
+function johnLennonFacts(facts) {
+  var strings = [],
+      counter = 0;
+      
+  while ( counter < facts.length ) {
+    strings.push(`${facts[counter]}!!!`);   
+  }
+  
+  return strings;
+}
